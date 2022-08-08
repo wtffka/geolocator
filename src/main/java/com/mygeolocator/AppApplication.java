85ac9d2e8a59d2fa5b0f687fb893b5d720403576
wtffka <bal1337@yandex.ru>
2022-08-09 02:44:12 +0300

    geolocator v1.0

--- src/main/java/com/mygeolocator/AppApplication.java
+++ src/main/java/com/mygeolocator/AppApplication.java
@@ -2,9 +2,8 @@ package com.mygeolocator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
